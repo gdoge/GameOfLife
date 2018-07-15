cf041c61145415fb615da5df1c3b2c4ee38ef8ec
test test <test@test.de>
2018-07-15 17:57:44 +0200

    first commit

---
+++ gameOfLife.js
@@ -0,0 +1,17 @@
+
+
+
+const boardSizeX;
+const boardSizeY;
+
+
+function makeBoard(boardSizeX, boardSizeY){
+
+
+    for (let i = 0; i < boardSizeX; i++ ){
+        for (let j = 0; j < boardSizeY; i++){
+            
+        }
+    }
+
+}
