5a74bce255b108fd8ae79a2d318cbc77e0c44f16
test test <test@test.de>
2018-07-15 18:59:50 +0200

    progress

--- gameOfLife.js
+++ gameOfLife.js
@@ -1,17 +1,27 @@
+const boardSizeX = 40;
+const boardSizeY = 20;
+let gameBoard = [];
 
+function makeBoard(X, Y) {
+    var body = document.body,
+        tbl = document.createElement('table');
+    tbl.style.width = '100px';
+    tbl.style.border = '1px solid black';
 
-
-const boardSizeX;
-const boardSizeY;
-
-
-function makeBoard(boardSizeX, boardSizeY){
-
-
-    for (let i = 0; i < boardSizeX; i++ ){
-        for (let j = 0; j < boardSizeY; i++){
-            
+    for (let i = 0; i < X; i++) {
+        console.log("1");
+        var tr = tbl.insertRow();
+        for (let j = 0; j < Y; j++) {
+            var td = tr.insertCell();
+            td.appendChild(document.createTextNode('Cell'));
+            document.getElementById("board").appendChild = '<td> O </td>'
         }
+        document.getElementById("board").appendChild = '</tr>'
     }
-
+    body.appendChild(tbl);
 }
+
+function initializeBoard() {
+}
+makeBoard(boardSizeX, boardSizeY);
+console.log("TEST");
