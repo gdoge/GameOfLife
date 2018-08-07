aff0b0fbbc752f790b4a05d13c5814badbe78ec6
test test <test@test.de>
2018-08-07 21:37:29 +0200

    refined algorythm to find neighbours

--- gameOfLife.js
+++ gameOfLife.js
@@ -1,6 +1,7 @@
 const boardSizeX = 40;
 const boardSizeY = 20;
 let gameBoard = [];
+let liveCells = [];
 
 function makeBoard(X, Y) {
     var body = document.body,
@@ -8,20 +9,58 @@ function makeBoard(X, Y) {
     tbl.style.width = '100px';
     tbl.style.border = '1px solid black';
 
-    for (let i = 0; i < X; i++) {
-        console.log("1");
+    for (let i = 0; i < Y; i++) {
         var tr = tbl.insertRow();
-        for (let j = 0; j < Y; j++) {
+        for (let j = 0; j < X; j++) {
             var td = tr.insertCell();
-            td.appendChild(document.createTextNode('Cell'));
-            document.getElementById("board").appendChild = '<td> O </td>'
+            td.id = j + '|' + i;
+            td.appendChild(document.createTextNode('O'));
         }
         document.getElementById("board").appendChild = '</tr>'
+    }
+    if (tbl != null) {
+        for (var i = 0; i < tbl.rows.length; i++) {
+            for (var j = 0; j < tbl.rows[i].cells.length; j++)
+                tbl.rows[i].cells[j].onclick = function () {
+                    setCell(this);
+                };
+        }
     }
     body.appendChild(tbl);
 }
 
 function initializeBoard() {
 }
+
+function setCell(tableCell) {
+    let count = getNumberOfNeighbours(tableCell);
+    tableCell.innerHTML = count;
+    liveCells.push(tableCell.id);
+    console.log(liveCells);
+}
+
+function getNumberOfNeighbours(tableCell) {
+    coordinates = tableCell.id.split("|");
+    neighbourCounter = 0;
+    bottomLeft = { x: coordinates[0] - 1, y: coordinates[1] - 1 }
+
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            tableElement = document.getElementById((bottomLeft.x + j) + "|" + (bottomLeft.y + i))
+            if (tableElement != null && tableElement.innerHTML != "O") {
+                neighbourCounter++;
+                console.log((bottomLeft.x + j) + "|" + (bottomLeft.y + i));
+            }
+        }
+    }
+
+    if(tableCell.innerHTML != "O") {
+        neighbourCounter --;
+    }
+
+    console.log(neighbourCounter);
+
+    return neighbourCounter;
+}
+
 makeBoard(boardSizeX, boardSizeY);
-console.log("TEST");
